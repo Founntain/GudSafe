1ad7c8e37f6dee1423ea0f23c5abb10a8246b1b3
Jan Engel <jan.engel@incowia.com>
2022-10-21 12:15:59 +0200

    init commit

---
+++ GudSafe.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace GudSafe.WebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
