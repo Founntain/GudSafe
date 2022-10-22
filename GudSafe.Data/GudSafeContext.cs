d9b6f4ab733e367c9298050e244080bfc1935394
Jan Engel <jan.engel@incowia.com>
2022-10-22 09:48:39 +0200

    commited missing files

---
+++ GudSafe.Data/GudSafeContext.cs
@@ -0,0 +1,25 @@
+﻿using GudSafe.Data.Configurations;
+using GudSafe.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+
+namespace GudSafe.Data;
+
+public class GudSafeContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<GudFile> Files { get; set; }
+    public DbSet<Collection> Collections { get; set; }
+    
+    public GudSafeContext()
+    {
+        Database.EnsureCreated();
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new UserConfiguration());
+        modelBuilder.ApplyConfiguration(new GudFileConfiguration());
+        modelBuilder.ApplyConfiguration(new GudCollectionConfiguration());
+    }
+}
