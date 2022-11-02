82e1b6f504ff9cabca8ef4ab1d50dd235dc39a13
Carola Kloss <2001fflp@gmail.com>
2022-11-02 17:34:31 +0100

    Add dark mode toggle

--- GudSafe.WebApp/wwwroot/js/site.js
+++ GudSafe.WebApp/wwwroot/js/site.js
@@ -2,3 +2,50 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+let isDark = (localStorage.getItem('isDark') || "false") === "true";
+
+$(function () {
+    let mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+    mediaQuery.onchange = function (e) {
+        toggleDarkMode(e.matches);
+    };
+})
+
+function toggleDarkMode(newVal) {
+    if (!newVal) {
+        newVal = !isDark;
+    }
+
+    if (newVal) {
+        $("html").addClass('alt-colors');
+    } else {
+        $("html").removeClass('alt-colors');
+    }
+    
+    updateIcons(newVal);
+
+    localStorage.setItem('isDark', newVal);
+    isDark = newVal;
+}
+
+function updateIcons(dark)
+{
+    let darkModeIcon = $("#dark-icon");
+    let darkModeToggle = $("#dark-toggle");
+
+    if (dark) {
+        darkModeIcon.removeClass('bi-sun');
+        darkModeToggle.removeClass('bi-toggle-on');
+
+        darkModeIcon.addClass('bi-moon');
+        darkModeToggle.addClass('bi-toggle-off');
+    } else {
+        darkModeIcon.removeClass('bi-moon');
+        darkModeToggle.removeClass('bi-toggle-off');
+
+        darkModeIcon.addClass('bi-sun');
+        darkModeToggle.addClass('bi-toggle-on');
+    }
+}
